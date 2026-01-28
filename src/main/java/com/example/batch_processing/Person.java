fbf0b3fbd1060012cd0c52d19d4d816b9da6f103
norsco <zakaria.norsco@gmail.com>
2026-01-28 19:24:15 +0100

    score crdss

--- src/main/java/com/example/batch_processing/Person.java
+++
@@ -1,9 +0,0 @@
-package com.example.batch_processing;
-
-public record Person (String firstName, String lastName) {
-
-    public Person(String firstName, String lastName) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-    }
-}
